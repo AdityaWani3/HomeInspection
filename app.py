79b2ec2a50219e9cdf7c4898394d85e49bb0217d
AdityaWani3 <adityawani333@gmail.com>
2025-04-10 13:51:55 +0530

    Update app.py

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ import time
 import json
 import google.generativeai as genai
 
-api_key = "AIzaSyA22LYGk5IOnCAktpwwySVsLAp97N6ZjNA"
+api_key = "AIzaSyBrtgnK9aaBhngeIF4sTfFsOh9o0PsQPBw"
 genai.configure(api_key=api_key)
 from pathlib import Path
 
